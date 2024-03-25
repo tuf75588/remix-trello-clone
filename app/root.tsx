f46879a9f44a008dd67d3ca445b100dde035057e
Andrew Davis <develop.it4@gmail.com>
2024-03-25 16:03:44 -0400

    Add IconLink component to root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,23 @@ export default function App({ children }: { children: React.ReactNode }) {
               <div className="font-black text-2xl text-white">Trellix</div>
               <div className="text-slate-500">a Remix Demo</div>
             </Link>
+            <div className="flex items-center gap-6">
+              <IconLink
+                href="https://www.youtube.com/watch?v=RTHzZVbTl6c&list=PLXoynULbYuED9b2k5LS44v9TQjfXifwNu&pp=gAQBiAQB"
+                icon="/yt_icon_mono_dark.png"
+                label="Videos"
+              />
+              <IconLink
+                href="https://github.com/remix-run/example-trellix"
+                label="Source"
+                icon="/github-mark-white.png"
+              />
+              <IconLink
+                href="https://remix.run/docs/en/main"
+                icon="/r.png"
+                label="Docs"
+              />
+            </div>
           </div>
         </div>
         <Outlet />
@@ -32,5 +49,25 @@ export default function App({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
+  );
+}
+
+function IconLink({
+  icon,
+  href,
+  label,
+}: {
+  icon: string;
+  href: string;
+  label: string;
+}) {
+  return (
+    <a
+      href={href}
+      className="text-slate-500 text-xs uppercase font-bold text-center"
+    >
+      <img src={icon} aria-hidden className="inline-block h-8" />
+      <span className="block mt-2">{label}</span>
+    </a>
   );
 }
